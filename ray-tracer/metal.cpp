caf286ce72f03c47f01373c048de9e2742cd21f0
LAPTOP-KHJ6N8HS\kinof <kinningchu624@gmail.com>
2023-03-22 12:37:28 +0800

    fuzzy metal fix

--- ray-tracer/metal.cpp
+++ ray-tracer/metal.cpp
@@ -4,7 +4,6 @@ bool metal::scatter(const ray& r_in, const hit_record& rec, color& attenuation, 
 {
     vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
     scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
-    scattered = ray(rec.p, reflected);
     attenuation = albedo;
     return (dot(scattered.direction(), rec.normal) > 0);
 }
